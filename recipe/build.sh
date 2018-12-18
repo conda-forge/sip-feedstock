223d18a644f93563eff116e265e8dad85b060a12
Carlos Cordoba <ccordoba12@gmail.com>
2018-12-18 16:43:04 -0500

    Tell Linux gcc build how to find the compiler

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# This block is taken from the PyQt recipe
+if [[ ${c_compiler} != "toolchain_c" ]]; then
+    if [[ ${HOST} =~ .*linux.* ]]; then
+        mkdir bin || true
+        pushd bin
+            ln -s ${GXX} g++ || true
+            ln -s ${GCC} gcc || true
+        popd
+        export PATH=${PWD}/bin:${PATH}
+    fi
+fi
+
 export LINK=${CC}
 python configure.py --sysroot=$PREFIX
 
