7106f66296b9e4258a3fc1829ad1b0ac45974a66
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2022-02-10 16:54:20 -0500

    Do not override flags

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,9 @@
 set -exou
 
 if [[ $(uname) == "Darwin" ]]; then
-    export CFLAGS="-arch x86_64 -arch arm64"
-    export CXXFLAGS="-arch x86_64 -arch arm64"
-    export LDFLAGS="-arch x86_64 -arch arm64"
+    export CFLAGS="$CFLAGS -arch x86_64 -arch arm64"
+    export CXXFLAGS="$CXXFLAGS -arch x86_64 -arch arm64"
+    export LDFLAGS="$LDFLAGS -arch x86_64 -arch arm64"
 fi
 
 $PYTHON -m pip install . -vv
