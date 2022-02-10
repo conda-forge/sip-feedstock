013a2a445dbdd5332975e159444d0c95dfdcaf52
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2022-02-10 17:42:49 -0500

    Universal binaries only on osx-arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 set -exou
 
-if [[ $(uname) == "Darwin" ]]; then
+if [[ "${target_platform}" == "osx-arm64" ]]; then
     export CFLAGS="$CFLAGS -arch x86_64 -arch arm64"
     export CXXFLAGS="$CXXFLAGS -arch x86_64 -arch arm64"
     export LDFLAGS="$LDFLAGS -arch x86_64 -arch arm64"
