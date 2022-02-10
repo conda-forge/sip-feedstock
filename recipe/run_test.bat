ae622d6c3d8c5cd083526f68548a924cff3dc85a
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2022-02-10 11:56:37 -0500

    Remove unused comments

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -1,13 +1,4 @@
 cd test
 
 sip-build
-:: sip -c . -b word.sbf word.sip
-:: if errorlevel 1 exit 1
-
-:: python configure.py
-:: if errorlevel 1 exit 1
-
-:: This requires running vcvarsall.bat previously,
-:: which is not done in the test phase
-:: nmake
-:: if errorlevel 1 exit 1
+if errorlevel 1 exit 1
--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-# export LINK=${CC}
 cd test
 sip-build
-# sip -c . -b word.sbf word.sip
-# python configure.py
-# make
